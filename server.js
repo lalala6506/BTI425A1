ab55c6056a73fc14dfeff2457339e8a93f0226ad
Cha Li <lifuzhang1991@gmail.com>
2025-01-13 16:38:45 -0500

    small update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
+const ListingsDB = require("./modules/listingsDB.js"); 
+const db = new ListingsDB();
 
 app.get("/", (req,res)=>{
     res.send("Vercel Works")
